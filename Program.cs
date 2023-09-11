340a32b21978aa0078b3ccb92e126522249e387d
Lutin <tclutin01@gmail.com>
2023-09-11 19:37:10 +0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Data;
+using System.Diagnostics;
+using System.Numerics;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.ExceptionServices;
+using System.Runtime.InteropServices;
+
+namespace Lab1_algorithm
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+       
+        }
+    }
+}
