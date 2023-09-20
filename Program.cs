b06594ca7f89bcad98f5ad509064c8eb21e2c4da
Lutin <tclutin01@gmail.com>
2023-09-20 19:09:41 +0500

    fix: main files was fixed

--- Program.cs
+++ Program.cs
@@ -10,90 +10,14 @@ namespace Lab1_algorithm
 {
     public class Program
     {
-        public static Random random = new Random();
-        public static Stopwatch stopWatch = new Stopwatch();
-
-        public static int[] GenerateVector(int n)
-        {
-            int[] vector = new int[n];
-            for (int i = 0; i < n; i++)
-            {
-                vector[i] = random.Next(1, 1000);
-            }
-            return vector;
-        }
-
-        public static long SumElementsVector(int[] vector)
-        {
-            long result = 0;
-            for (int i = 0; i < vector.Length; i++)
-            {
-                result += vector[i];
-            }
-            return result;
-        }
-
-        public static long MultiplyElementsVector(int[] vector)
-        {
-            long result = 1;
-            for (int i = 0; i < vector.Length; i++)
-            {
-                result *= vector[i];
-            }
-            return result;
-        }
-
-        public static int[] BubbleSort(int[] vector)
-        {
-            for (int i = 0; i < vector.Length - 1; i++)
-            {
-                for (int j = 0; j < vector.Length - i - 1; j++)
-                {
-                    if (vector[j] > vector[j + 1])
-                    {
-                        int n = vector[j];
-                        vector[j] = vector[j + 1];
-                        vector[j + 1] = n;
-                    }
-                }
-            }
-            return vector;
-        }
-
-        public static void PrintVector(int[] vector)
-        {
-            foreach (var item in vector)
-            {
-                Console.WriteLine(item);
-            }
-        }
-
-        public static double Run(int n, Func<int[], int[]> operation)
-        {
-            int[] vector = GenerateVector(n);
-            stopWatch.Reset();
-            stopWatch.Start();
-            operation(vector);
-            stopWatch.Stop();
-            return stopWatch.Elapsed.TotalSeconds;
-        }
 
         public static void Main(string[] args)
         {
-            Console.WriteLine("Bubbler2004: ");
-            for (int n = 1; n <= 2000; n++)
-            {
-                double totalTime = 0;
-                for (int i = 0; i < 5; i++)
-                {
-                    double runTime = Run(n, BubbleSort);
-
-                    totalTime += runTime;
-                    Console.WriteLine($"n = {n}, time = {runTime.ToString("F8")} seconds");
-                }
-                double averageTime = totalTime / 5;
-                Console.WriteLine($"Average time for item = {n}: {averageTime.ToString("F8")} seconds");
-            }
+            Measurement measurement = new Measurement();
+            measurement.Run("Tim", 2000, 5, Algorithms.Sort.TimSort);
+            measurement.Run("Merge", 2000, 5, Algorithms.Sort.MergedSort);
+            measurement.Run("Bubble", 2000, 5, Algorithms.Sort.BubbleSort);
+            measurement.Run("Sum", 2000, 5, Algorithms.Arithmetic.SumElementsVector);
         }
     }
 }
